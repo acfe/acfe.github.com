4c62d54e949afb6ab85fb164f8d648ba7b5ef055
unknown <001264@FC001264PC1.fcbox.com>
2017-08-11 20:53:50 +0800

    add control

--- tree/show_index.js
+++ tree/show_index.js
@@ -15,7 +15,7 @@ index.prototype =  {
         var show = this.getQueryString('show', location.search);
         show = show || 'index';
         var loadArr = [];
-        loadArr.push(publicPath + show + version + '.js?0.4645917871523575');
+        loadArr.push(publicPath + show + version + '.js?0.19890242977596162');
         require(loadArr);
     },
 
