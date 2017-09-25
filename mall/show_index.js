d226e65303b307eac91a65c62309902a3e42a820
程小佳(001264) <001264@fcbox.com>
2017-09-25 11:19:00 +0800

    mo mall

--- mall/show_index.js
+++ mall/show_index.js
@@ -15,7 +15,7 @@ index.prototype =  {
         var show = this.getQueryString('show', location.search);
         show = show || 'index';
         var loadArr = [];
-        loadArr.push(publicPath + show + version + '.js?0.6627334200042398');
+        loadArr.push(publicPath + show + version + '.js?0.10805013898358773');
         require(loadArr);
     },
 
