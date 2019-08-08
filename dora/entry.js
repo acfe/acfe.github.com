d4f2bca9aa04ab39527b9dd1e22e125672596f9c
程小佳(001264) <001264@fcbox.com>
2019-08-08 19:22:01 +0800

    mo path

--- dora/entry.js
+++ dora/entry.js
@@ -1,4 +1,4 @@
-const publicPath = '/'
+const publicPath = '/dora'
 
 var Index = function () {
   this.commonInit()
@@ -42,7 +42,7 @@ Index.prototype = {
     // loading
     var app = document.getElementById('app')
     app.style.minHeight = document.clientHeight + 'px'
-    app.innerHTML = '<div class="loadingImg"><img src="/img/loading.gif"></div>'
+    app.innerHTML = '<div class="loadingImg"><img src="/doraimg/loading.gif"></div>'
     this.createCss(show)
     setTimeout(function () {
       var modernBrowser = ('fetch' in window && 'assign' in Object)
@@ -73,14 +73,14 @@ Index.prototype = {
     link.rel = 'stylesheet'
     link.type = 'text/css'
     head.appendChild(link)
-    link.href = publicPath + 'css/' + name + '.css?0.09890970177359737'
+    link.href = publicPath + 'css/' + name + '.css?0.42738568274673416'
   },
 
   createJs: function (name, callback) {
     var head = document.getElementsByTagName('head')[0]
     var script = document.createElement('script')
     script.async = false
-    script.src = publicPath + 'js/' + name + '.js?0.09890970177359737'
+    script.src = publicPath + 'js/' + name + '.js?0.42738568274673416'
     head.appendChild(script)
     script.onload = function () {
       callback && callback()
